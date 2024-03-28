06985dd55d1976acbb4a1598df39614d0a377f05
k2supra <andrijkorolevic@gmail.com>
2024-03-28 11:55:37 +0200

    Queue

--- Structures/ex_24/main.cpp
+++ Structures/ex_24/main.cpp
@@ -27,6 +27,98 @@ public:
 		this->tail = nullptr;
 	};
 
+	Queue(const Queue& other)
+	{
+		this->head = nullptr;
+		this->tail = nullptr;
+		Node<T>* temp = other.head;
+		while (temp != nullptr)
+		{
+			addLastQueue(temp->data);
+			temp = temp->next;
+		}
+	}
+
+	int getSize()
+	{
+		int size = 0;
+
+		Node<T>* temp = head;
+
+		if (temp == nullptr)
+		{
+			cout << "Impossible";
+		}
+		while (temp)
+		{
+			size++;
+			temp = temp->next;
+		}
+		
+		return size;
+	}
+
+	/*Queue operator=(const Queue& other)
+	{
+		while (!isEmpty())
+		{
+			deleteFirstQueue();
+		}
+
+		Node<T>* temp = other.head;
+		while (temp != nullptr)
+		{
+			addLastQueue(temp->data);
+			temp = temp->next;
+		}
+		return *this;
+	}*/
+
+	Queue operator+(const Queue& other)
+	{
+		Queue newQueue;
+
+		Node<T>* temp = other.head;
+
+		while (temp != nullptr)
+		{
+			newQueue.addLastQueue(temp->data);
+			temp = temp->next;
+		}
+
+		temp = head;
+		while (temp != nullptr)
+		{
+			newQueue.addLastQueue(temp->data);
+			temp = temp->next;
+		}
+
+		delete temp;
+		return newQueue;
+	}
+
+	Queue operator*(const Queue& other)
+	{
+		Queue result;
+		Node<T>* temp1 = head;
+		while (temp1 != nullptr)
+		{
+			Node<T>* temp2 = other.head;
+			while (temp2 != nullptr)
+			{
+				if (temp1->data == temp2->data)
+				{
+					result.addLastQueue(temp1->data);
+					break;
+				}
+				temp2 = temp2->next;
+			}
+			temp1 = temp1->next;
+		}
+		delete temp1;
+		return result;
+	}
+
 	bool isEmpty()
 	{
 		return head == nullptr;
@@ -135,17 +227,20 @@ private:
 
 int main()
 {
+	cout << "1" << endl;
+
 	Queue<int> q1;
 
+	
 	q1.addLastQueue(11);
-	q1.addLastQueue(22);
-	q1.addLastQueue(33);
-	q1.addLastQueue(44);
-	q1.addLastQueue(55);
+	q1.addLastQueue(12);
+	q1.addLastQueue(13);
+	q1.addLastQueue(14);
+	q1.addLastQueue(15);
 
 	q1.printQueue();
 
-	q1.addFirstQueue(99);
+	q1.addFirstQueue(19);
 
 	q1.printQueue();
 
@@ -157,5 +252,43 @@ int main()
 	q1.getFirstQueue();
 	q1.getLastQueue();
 
-	q1.~Queue();
+
+	cout << "2" << endl;
+
+	Queue<int> q2(q1);
+
+	q2.addLastQueue(29);
+
+	q2.printQueue();
+
+
+	cout << "3" << endl;
+
+	Queue<int> q3 = q2;
+
+	q3.deleteFirstQueue();
+
+	q3.printQueue();
+
+
+	cout << "4" << endl;
+
+	Queue<int> q4 = q1 + q3;
+
+	q4.deleteLastQueue();
+
+	q4.printQueue();
+
+	cout << "5" << endl;
+
+	Queue<int> q5 = q2 * q3;
+
+	q5.printQueue();
+
+	cout << endl;
+
+	cout << "Size1: " << q1.getSize() << endl;
+	cout << "Size2: " << q2.getSize() << endl;
+	cout << "Size3: " << q3.getSize() << endl;
+	cout << "Size4: " << q4.getSize() << endl;
 };
